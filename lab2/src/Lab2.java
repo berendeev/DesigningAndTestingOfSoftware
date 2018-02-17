a3bd5c079f8fb29215326250e98c7430d0fb15f2
berendeev <sellect.47@gmail.com>
2018-02-17 10:53:54 +0600

    rewite lab2 with using Set

--- lab2/src/Lab2.java
+++ lab2/src/Lab2.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Lab2 {
 	private String string;
 
@@ -7,11 +10,19 @@ public class Lab2 {
 
 	public int count() {
 		int count = 0;
-		String vowels = "аоиеёэыуюя";
+		Set<Character> vowels = new HashSet<>();
+		vowels.add('а');
+		vowels.add('о');
+		vowels.add('и');
+		vowels.add('е');
+		vowels.add('ё');
+		vowels.add('э');
+		vowels.add('ы');
+		vowels.add('у');
+		vowels.add('ю');
+		vowels.add('я');
 		for (int i = 0; i < string.length(); i++) {
-			for (int j = 0; j < vowels.length(); j++) {
-				if (string.toLowerCase().charAt(i) == vowels.charAt(j)) count++;
-			}
+			if(vowels.contains(string.toLowerCase().charAt(i)))count++;
 		}
 		return count;
 	}
